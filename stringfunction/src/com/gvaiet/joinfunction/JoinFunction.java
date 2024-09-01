842bcb488a8b45e41d4a6dd7b97cf059393734a7
tribh <tribh@Sidharth_123>
2024-09-01 19:51:37 +0530

    commit

---
+++ stringfunction/src/com/gvaiet/joinfunction/JoinFunction.java
@@ -0,0 +1,23 @@
+package com.gvaiet.joinfunction;
+
+
+public class JoinFunction {
+
+	public static void main(String[] args) {
+		
+	 String address = "shelu GVAIET";
+	
+	 String[] addComponent = address.split(" ");
+	
+	 String newAddress = String.join("->", addComponent);
+	
+	System.out.println(newAddress);
+	}
+}
+
+//matches(), 
+//toCharArray(), 
+//split(), 
+//join(), 
+//charAt(), 
+//length()
