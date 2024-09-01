9f3394b97f5203a2c0024a00b809693b6409c017
tribh <tribh@Sidharth_123>
2024-09-02 00:06:35 +0530

    commit

---
+++ streamapi/src/com/gvaiet/streamapi/assignment1/pincode/Pincode.java
@@ -0,0 +1,29 @@
+package com.gvaiet.streamapi.assignment1.pincode;
+public class Pincode {
+
+	private int pincode;
+
+	public Pincode(int pincode) {
+		super();
+		this.pincode = pincode;
+	}
+
+	public Pincode() {
+		super();
+	}
+
+	public int getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+	@Override
+	public String toString() {
+		return "Pincode [pincode=" + pincode + "]";
+	}
+	
+	
+}
