68d30f9eae23d0472614de50e6cfb8b7b8019feb
tribh <tribh@Sidharth_123>
2024-09-02 00:30:00 +0530

    bdhdj

---
+++ swaptwonumbers1/src/com/gvaiet/swaptwonumbers1/SwapTwoNumbersMain.java
@@ -0,0 +1,12 @@
+package com.gvaiet.swaptwonumbers1;
+
+public class SwapTwoNumbersMain {
+
+	public static void main(String[] args) {
+		SwapTwoNumbers swaptwonumber = new SwapTwoNumbers(10,20);
+		
+		swaptwonumber.numbersBeforeSwap();
+		swaptwonumber.swapTwoNumbersUsingThirdVariable();
+		swaptwonumber.swapTwoNumbersWithoutUsingThirdVariable();
+	}
+}
